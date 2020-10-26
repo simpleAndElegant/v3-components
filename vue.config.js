72bca81828dce92403292553519728acfab3d832
liangrui3 <liangrui3@tal.com>
2020-10-26 18:23:33 +0800

    feat: 路由实现

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,25 @@
+const path = require('path');
+
+const resolve = (filePath) => path.join(__dirname, './', filePath);
+
 module.exports = {
   outputDir: 'docs',
   publicPath: '/v3-components/',
+  devServer: { port: '3366' },
+  pages: {
+    index: {
+      entry: resolve('story/main.ts'),
+      template: 'public/index.html',
+      filename: 'index.html',
+      title: 'v3-components',
+    },
+  },
+  chainWebpack: (config) => {
+    config.plugins
+      .delete('prefetch-index')
+      .delete('preload-index');
+    config.resolve.alias
+      .set('story', resolve('story'));
+  },
+
 };
